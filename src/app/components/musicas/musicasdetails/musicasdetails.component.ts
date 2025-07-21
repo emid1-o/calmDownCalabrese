e66f0d2adac4e4ee0cb3be69a8aaac0dcb52d26c
emid1-o <emidiolop5@gmail.com>
2025-07-21 15:47:41 -0300

    funciona

--- src/app/components/musicas/musicasdetails/musicasdetails.component.ts
+++ src/app/components/musicas/musicasdetails/musicasdetails.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Musica } from '../../../models/musica';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-musicasdetails',
@@ -11,11 +13,46 @@ import { Musica } from '../../../models/musica';
 })
 export class MusicasdetailsComponent {
 
-  musica:Musica = new Musica(0, "");
+  router = inject(ActivatedRoute);
+  router2 = inject(Router);
 
-  save(){
+  constructor() {
+    let id = this.router.snapshot.params["id"];
+    if (id > 0) {
+      this.findById(id)
+    }
+  }
 
-    alert("foi salvo confia");
+  findById(id: number) {
+    let musicaRetornada: Musica = new Musica(id, "musica");
+    this.musica = musicaRetornada;
+
+  }
+
+  musica: Musica = new Musica(0, "");
+
+  save() {
+    if (this.musica.id > 0) {
+
+      Swal.fire({
+        title: "Sucesso",
+        text: "Editado com sucesso",
+        icon: "success",
+        confirmButtonText: 'Ok'
+      });
+      this.router2.navigate(["admin/musicas"], { state: { musicaEditada: this.musica } });
+
+    } else {
+
+      Swal.fire({
+        title: "Sucesso",
+        text: "Salvo com sucesso",
+        icon: "success",
+        confirmButtonText: 'Ok'
+      });
+
+      this.router2.navigate(["admin/musicas"], { state: { musicaNova: this.musica } });
+    }
   }
 
 }
--- src/app/components/musicas/musicaslist/musicaslist.component.ts
+++ src/app/components/musicas/musicaslist/musicaslist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Musica } from '../../../models/musica';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-musicaslist',
@@ -10,16 +11,49 @@ import { RouterLink } from '@angular/router';
 })
 export class MusicaslistComponent {
 
-  lista:Musica[] = [];
+  lista: Musica[] = [];
 
-  constructor(){
+  constructor() {
 
     this.lista.push(new Musica(1, "californication"))
     this.lista.push(new Musica(2, "No surprises"))
     this.lista.push(new Musica(3, "All your sisters"))
+
+    let musicaNova = history.state.musicaNova;
+    let musicaEditada = history.state.musicaEditada;
+
+    if (musicaNova) {
+      musicaNova.id = 555;
+      this.lista.push(musicaNova);
+    }
+
+    if (musicaEditada) {
+
+      let indice = this.lista.findIndex(x => { return x.id == musicaEditada.id });
+      this.lista[indice] = musicaEditada;
+    }
   }
 
-  editar(){}
-  deletar(){}
+
+  deleteById(musica: Musica) {
+    Swal.fire({
+      title: "Tem certeza que deseja deletar a música??",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Deletar",
+      denyButtonText: `Não deletar`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        let indice = this.lista.findIndex(x => { return x.id == musica.id });
+        this.lista.splice(indice, 1);
+        Swal.fire("Deletado!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Não deletado", "", "info");
+      }
+    });
+
+
+  }
 
 }
